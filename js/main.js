b532da78bff81f236059b46698701651df9358ee
Александр <sh889@ya.ru>
2022-02-14 16:20:03 +0500

    Адаптив

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 const swiper = new Swiper('.main-slider', {
-  slidesPerView: 2,
   spaceBetween: 30,
   loop: true,
   navigation: {
     nextEl: '.main-slider__arrow',
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    }
+  },
+});
+
+const burger = document.querySelector('.menu-burger');
+const closeMenu = document.querySelector('.header__menu-close');
+const menuMobile = document.querySelector('.header');
+
+burger.addEventListener('click', () => {
+  menuMobile.style.display = 'block';
+});
+
+closeMenu.addEventListener('click', () => {
+  menuMobile.style.display = 'none';
 });
